c0a4a127d5bc95c8036fd8100137a80accd257c7
Ozer <maozerulusoy@gmail.com>
2016-05-03 17:28:48 +0200

    dungeon file met daarin node edge classes
    jwz

---
+++ ConsoleApplication1/ConsoleApplication1/Dungeon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softtest
+{
+    class Dungeon
+    {
+
+    }
+
+    abstract class OgNode
+    {
+        protected int maxMonsters, nodeLevel,healthPotions, timeCrystals;
+    }
+
+    class BeginNode : OgNode 
+    {
+        Edge nextEdge; 
+        public BeginNode(Node nNext)
+        {
+            nextEdge = new Edge(this,nNext);
+            maxMonsters = nodeLevel = 0;
+        }
+    }
+
+    class EndNode : OgNode
+    {
+        public EndNode()
+        {
+            maxMonsters = 0;
+            nodeLevel = 0;
+            healthPotions = 0;
+            timeCrystals = 0;
+        }
+    }
+
+    class Node:OgNode
+    {
+        float m;
+        bool contested;
+        Edge north, south, east, west;
+
+        public Node(int nLevel,Node nNorth,Node nSouth,Node nEast,Node nWest)
+        {
+            nodeLevel = nLevel;
+            m = 0.9f;
+            maxMonsters = (int)m*(nodeLevel+1);
+            north = new Edge(this,nNorth);
+            south = new Edge(this,nSouth);
+            east  = new Edge(this,nEast);
+            west  = new Edge(this,nWest);
+        }
+ 
+    }
+
+    class Edge
+    {
+        OgNode node1;
+        OgNode node2;
+        bool isEmpty = false;
+
+        public Edge(OgNode n1, OgNode n2)
+        {
+            node1 = n1;
+            node2 = n2;
+            if (node2 == null)
+                isEmpty = true;
+        }
+    }
+}
